2528b613caff645b3c636b1d5093edc62b049d79
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-10-02 22:50:27 +0000

    change tabs

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # replace a setting line with puppet
 exec { 'replace a line':
-     command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" var/www/html/wp-settings.php\',
+  command => '/bin/bash/sudo /bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" var/www/html/wp-settings.php\',
      }
