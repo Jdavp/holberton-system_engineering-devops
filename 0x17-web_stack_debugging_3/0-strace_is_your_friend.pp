73b0cb872e43b3e53dd0aec41b3be0faf6d46f1d
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-10-02 22:58:29 +0000

    change sudo location

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # replace a setting line with puppet
-exec { 'change a line':
+exec { 'replace a setting line':
   command => '/usr/bin/sudo /bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
     }
