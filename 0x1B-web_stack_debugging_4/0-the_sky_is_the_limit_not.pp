f46dc4e447077ce120f83edd2819b5e3dd6fbfe2
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-10-10 22:25:09 +0000

    fix numbers

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
-# change line with puppet
+B# change line with puppet
 exec { 'replace a setting line':
-  command  => '/usr/bin/sudo /
-  /bin/sed -i "s/n- 15/-n 5000/g" /etc/default/nginx',
+  command  => 'sed -i "s/15/5000/g" /etc/default/nginx',
   provider => shell
   }->
 exec { 'restart-nginx':
