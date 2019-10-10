d6be43b67a13ee4df2c607304266bc5b03447b0f
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-10-10 22:08:52 +0000

    add ->

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 exec { 'replace a setting line':
   command  => '/usr/bin/sudo /
-  /bin/sed -i "s/n- 15/-n 2000/g" /etc/default/nginx',
+  /bin/sed -i "s/n- 15/-n 5000/g" /etc/default/nginx',
   provider => shell
-  }
+  }->
 exec { 'restart-nginx':
   command => 'sudo service nginx restart',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
