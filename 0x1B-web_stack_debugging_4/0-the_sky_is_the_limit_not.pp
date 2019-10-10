a9c45fe6140d30fa0e526fb6d766be6de1c4d1da
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-10-10 21:55:55 +0000

    fix 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,3 @@
 exec { 'replace a setting line':
-  command => '/usr/bin/sudo /bin/sed -i "s/ULIMIT= '-n 15'/ULIMIT='-n 2000'/g" /etc/default/nginx',
+  command => '/usr/bin/sudo /bin/sed -i "s/n- 15/-n 2000/g" /etc/default/nginx',
   }
