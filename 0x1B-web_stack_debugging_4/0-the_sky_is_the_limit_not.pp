f650428cfa9c79aebda2d6f30666888a3c7a3648
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-10-10 22:06:33 +0000

    add restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,7 @@ exec { 'replace a setting line':
   /bin/sed -i "s/n- 15/-n 2000/g" /etc/default/nginx',
   provider => shell
   }
+exec { 'restart-nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+  }
