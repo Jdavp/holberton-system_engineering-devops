49c0b9e19d58db2f640653ee51e255d45dc6a08e
juan diego alejandro valencia pena <juandiegoalejandro@hotmail.com>
2019-10-10 23:34:03 +0000

    fix puppetlint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # replace line at os level
 exec { 'replace a setting soft':
-  command     => 'sed -i "s/5/30048/g" /etc/security/limits.conf',
-  provider    => shell,
+  command  => 'sed -i "s/5/30048/g" /etc/security/limits.conf',
+  provider => shell,
   }->
 exec { 'replace a setting hard':
-  command     => 'sed -i "s/4/10048/g" /etc/security/limits.conf',
-  provider    => shell, 
-  }->
+  command  => 'sed -i "s/4/10048/g" /etc/security/limits.conf',
+  provider => shell,
+}->
 exec { 'replace a setting line':
-  command     => 'sysctl -p',
-  provider    => shell, 
-  }
+  command  => 'sysctl -p',
+  provider => shell,
+}
